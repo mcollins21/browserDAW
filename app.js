aee6d08045bf2bb26fc77750462ad7a77bf6e17a
mcollins21 <mcollins21@luc.edu>
2023-11-20 20:18:31 -0600

    app.js update

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+
+// importing samples from samples.json
+
+/*
 let promiseSamples = loadSamplesData("samples.json");
 
 function makeSamples(obj) {
     
 }
 
+*/
+
+
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
+
+
+/*
+// initalizing tone.js transport
+Tone.Transport.bpm.value = 120;
+
+Tone.Transport.start();
+
+*/
